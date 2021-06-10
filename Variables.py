6c02b9500c922e021c54635eb6254a2b711d8e67
virajpatil483 <83685123+virajpatil483@users.noreply.github.com>
2021-06-10 23:28:20 +0530

    Delete Variables.py

--- Variables.py
+++
@@ -1,7 +0,0 @@
-nav = "Viru"
-school = "KCT Krishna School Karad "
-STD = 10 
-bestfriend = " Shourya , Yash "
-
-print('My Nick Name Is {} , \n I Have Studied In {} , \n Currently In {} STD  , \n My Best Friends or jaan {} \n  ' .format( nav, school, STD  , bestfriend ) ) 
-
