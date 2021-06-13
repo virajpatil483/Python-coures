97ae23a40be98c9dddbaa0c63dda344955ffc9fc
virajpatil483 <83685123+virajpatil483@users.noreply.github.com>
2021-06-13 14:52:52 +0530

    Add files via upload

---
+++ Day 3/Age calculator.py
@@ -0,0 +1,7 @@
+brithYear = int(input("Enter Your Brith Year:"))
+
+Age =  2021 - brithYear # 2021 FOR THE CURRENT AGE 
+
+print("YOU ARE {} YEARS OLD " .format(Age))
+
+
---
+++ Day 3/Calulator .py
@@ -0,0 +1,16 @@
+A = int(input("Your first no. sir"))
+
+B = int(input("Your Second no. sir"))
+
+print(A+B , "Addition of  Both the no.")
+print(A*B , "Multipication of Both the no.")
+print(A-B , "Substraction of  Both the no.")
+print(A/B , "Division of Both the no.")
+
+
+
+
+
+
+
+
